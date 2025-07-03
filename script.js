e35e0b19ccd4bb644a14af9cc0b06743f5ea057c
Parth6199 <suharth046@gmail.com>
2025-07-03 16:34:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById("subscriberForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const form = document.getElementById("subscriberForm");
+
+  const formData = {
+    Name: form.Name.value,
+    Designation: form.Designation.value,
+    Department: form.Department.value,
+    AutoTel: form.AutoTel.value,
+    Phone: form.Phone.value,
+    RequestType: form.RequestType.value,
+    ComplaintType: form.ComplaintType ? form.ComplaintType.value : '',
+    ComplaintDetails: form.ComplaintDetails ? form.ComplaintDetails.value : '',
+    Status: "Pending"
+  };
+
+  const scriptURL = "https://script.google.com/macros/s/AKfycbw72Pj1hBHgv_69B8mhseZI3jvNauKpKc0CgFZ_oO9sLYjSnKekrP8g9quwtm3sZrXVqA/exec";
+
+  fetch(scriptURL, {
+    method: "POST",
+    body: JSON.stringify(formData)
+  })
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("responseMsg").textContent = "✅ Submitted successfully!";
+      form.reset();
+      toggleComplaintFields();
+    })
+    .catch((err) => {
+      console.error("Error!", err.message);
+      document.getElementById("responseMsg").textContent = "❌ Something went wrong.";
+    });
+});
